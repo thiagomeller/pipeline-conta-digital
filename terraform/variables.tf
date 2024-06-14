d0cdd3c866599da00ae3e262ef48f44724db350c
Thiago Meller <thiagomellercasagrande@gmail.com>
2024-06-13 22:56:40 -0300

    landing zone script

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "learn-c5e8a9ad-16f9-4985-aaa1-7bd1ae16cf92"
+}
+
+variable "location" {
+  default = "westus"
+}
