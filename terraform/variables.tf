2f28b51779d8e56bd69c26047973033e6f06237a
Thiago Meller <thiagomellercasagrande@gmail.com>
2024-06-25 00:20:51 -0300

    gold

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 
 # Lista de variaveis utilizadas nos arquivos de terraform
 variable "resource_group_name" {
-  default = "learn-c5e8a9ad-16f9-4985-aaa1-7bd1ae16cf92"
+  default = "engDados"
 }
 
 variable "location" {
-  default = "westus"
+  default = "brazilsouth"
 }
